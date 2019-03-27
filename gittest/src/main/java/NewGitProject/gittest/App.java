4c53e80a6b146cf778b63047d2b2ad29f281ebce
Hari <haridol86@gmail.com>
2019-03-27 18:23:54 -0400

    Test Github

---
+++ gittest/src/main/java/NewGitProject/gittest/App.java
@@ -0,0 +1,20 @@
+package NewGitProject.gittest;
+
+import org.testng.annotations.Test;
+
+public class App {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+		
+	}
+
+}
